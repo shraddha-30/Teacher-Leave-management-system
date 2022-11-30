e19b1fcd67173eed89686d0b0f67c9bc47444cbe
shraddha dabade <118508539+shraddha-30@users.noreply.github.com>
2022-11-30 19:41:15 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | teacher Leave Management System in PHP | Developed By <a href="https://codeastro.com">shraddha dabade</a></p>
+        </div>
+</footer>
